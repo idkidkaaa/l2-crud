c3554b995c95f4886e2bc6eb149e2cabf778ce23
idkidkaaa <95030943+idkidkaaa@users.noreply.github.com>
2023-01-09 18:55:53 +0000

    Update setlike.php

--- setlike.php
+++ setlike.php
@@ -2,10 +2,8 @@
 
 include 'db.php';
 
-
-	$sql = ("UPDATE lab2 SET lk = lk + 1 WHERE id = ?");
-	$query = $pdo -> prepare($sql);
-	$query-> execute([$_GET['id']]);
+$sql = ("UPDATE lab2 SET lk = lk + 1 WHERE id = ?");
+$query = $pdo->prepare($sql);
+$query->execute([$_GET['id']]);
 	
-	header("Location:http://localhost/lab2/index.php");
-
+header("Location:index.php");
