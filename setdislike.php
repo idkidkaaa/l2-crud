c1ffa67fe5ac27dd23c3c1ad80cb1778c219ed3a
idkidkaaa <95030943+idkidkaaa@users.noreply.github.com>
2023-01-09 18:57:58 +0000

    Update setdislike.php

--- setdislike.php
+++ setdislike.php
@@ -3,7 +3,7 @@
 include 'db.php';
 
 $sql = ("UPDATE lab2 SET dlk = dlk + 1 WHERE id = ?");
-$query = $pdo -> prepare($sql);
+$query = $pdo->prepare($sql);
 $query-> execute([$_GET['id']]);
 	
 header("Location:index.php");
